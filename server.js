1887e5f74d518c8ebca00bd2a4ecb0d5bab2e03e
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-02-26 04:04:08 +0900

    Added babel and nodemon

--- server.js
+++ server.js
@@ -6,7 +6,22 @@ const handleHome = (req, res) => {
   res.send("Home");
 };
 
-app.get("/", handleHome);
+const handleVideo = (req, res) => {
+  res.send("Watch Video");
+};
+
+const handleProfile = (req, res) => {
+  res.send("Edit Profile");
+};
+
+const testMiddleware = (req, res, next) => {
+  console.log("Middleware test");
+  next();
+};
+
+app.get("/", testMiddleware, handleHome);
+app.get("/video", testMiddleware, handleVideo);
+app.get("/profile", handleProfile);
 
 const handleListen = () => {
   console.log("Connected localhost:4000 ✅");
