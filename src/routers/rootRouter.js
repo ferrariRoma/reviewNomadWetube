cfc0ab2c85ee345679ab046de8d3eeccdcc49bf4
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-03-26 04:32:21 +0900

    Fixed mistake about import postLogin handler and added post at login

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { home, searchVideo } from "../controllers/videoController";
-import { getJoin, postJoin, getLogin } from "../controllers/userController";
+import {
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+} from "../controllers/userController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.get("/search", searchVideo);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 
 export default rootRouter;
